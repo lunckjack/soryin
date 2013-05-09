b38cb75e135cb791d5dbe1df759d0d4333499996
lanqiang <disuzx@163.com>
2013-05-09 10:55:46 +0800

    modify alloy file

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,17 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+//颜色
+Alloy.Globals.green='2ECC71';//绿色
+Alloy.Globals.board_gray='CBCBCB'; //边框灰色
+Alloy.Globals.btn_orange-'F39C12';//按钮橙色
+Alloy.Globals.text_celadon='7F8C8D';//文字灰绿色
+Alloy.Globals.bar_gray='4A4A4A';//bar灰色
+
+
+//测试数据
+Alloy.Globals.testKey='';
+Alloy.Globals.requestURL='http://www.soryin.com:8080/soryin_beta/';
+Alloy.Globals.testLat='23.141028';//岗顶
+Alloy.Globals.testLng='113.34847';
